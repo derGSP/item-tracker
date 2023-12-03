e58b0a66a77a848efe87626f5760bcde134b3e01
Gerrit Plehn <gerrit@plehn.me>
2023-12-03 21:27:35 +0100

    disable migration from code

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
+// import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 import { env } from "~/env";
 import * as schema from "./schema";
@@ -7,4 +7,4 @@ import * as schema from "./schema";
 const client = postgres(env.DATABASE_URL, { prepare: false });
 export const db = drizzle(client, { schema });
 
-await migrate(db, { migrationsFolder: "drizzle" });
+// await migrate(db, { migrationsFolder: "drizzle" });
