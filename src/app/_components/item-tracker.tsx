e2b4497665fb7bb2fa6f2db4d44171845a23526f
Gerrit Plehn <gerrit@plehn.me>
2023-12-16 23:36:49 +0100

    Styling update

--- src/app/_components/item-tracker.tsx
+++ src/app/_components/item-tracker.tsx
@@ -67,10 +67,17 @@ export function ItemTracker(props: ItemProps) {
                   setSelectedPreset(i);
                 }}
                 checked={selectedPreset === i}
-                className=""
+                className="peer hidden"
               />
-              <label htmlFor={`presetRadio${i}`}>
-                {preset.name ?? preset.amount + (props.unit ?? "")}
+              <label
+                htmlFor={`presetRadio${i}`}
+                className="inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-white p-5 text-gray-500 hover:bg-gray-100 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-blue-600 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:peer-checked:text-blue-500"
+              >
+                <div className="block">
+                  <div className="w-full text-lg font-semibold">
+                    {preset.name ?? preset.amount + (props.unit ?? "")}
+                  </div>
+                </div>
               </label>
             </div>
           ))}
@@ -82,13 +89,13 @@ export function ItemTracker(props: ItemProps) {
                 setAmount(z.coerce.number().parse(e.target.value));
                 setSelectedPreset(-1); // Clear the selected preset when custom amount changes
               }}
-              className="w-full rounded-full px-4 py-2 text-black"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
             />
           )}
         </fieldset>
         <button
           type="submit"
-          className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
+          className="mb-2 me-2 rounded-full bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           disabled={logConsumption.isLoading}
         >
           {logConsumption.isLoading ? "Submitting..." : "Submit"}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import { ItemPanel } from "./_components/item-panel";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50 dark:bg-gray-900">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
           Tracking Moppet
         </h1>
 
