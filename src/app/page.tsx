fdc9430293738585d8439020799e36311c8855ce
Gerrit Plehn <gerrit@plehn.me>
2023-12-17 12:11:10 +0100

    Add Moppet explanation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,13 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50 dark:bg-gray-900">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-          Tracking Moppet
+          Tracking{" "}
+          <span
+            className="underline decoration-dotted"
+            title='colloq. a general "thing" in German'
+          >
+            Moppet
+          </span>
         </h1>
 
         <ItemPanel
