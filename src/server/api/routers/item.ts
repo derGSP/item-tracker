02f8c1db0c784c1444b1bdb933dc0ddf00121b41
derGSP <lukas@rittierodt.de>
2025-04-17 21:24:10 +0200

    bugfixes2

--- src/server/api/routers/item.ts
+++ src/server/api/routers/item.ts
@@ -82,7 +82,7 @@ export const itemRouter = createTRPCRouter({
           WHERE
             item = ${getDbName(input.item, input.verb)}
             AND time > ${input.from}::date
-            AND time <= ${input.to}::date
+            AND time <= ${input.to}
         )
       SELECT
         dates.date AS time,
